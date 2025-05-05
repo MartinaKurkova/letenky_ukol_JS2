6a7ce0f4871e37077414752e86d046c3df3e9c77
Martina Kurkova <martinanoha@seznam.cz>
2025-05-05 17:02:24 +0200

    Oprava

--- src/components/Flight/index.jsx
+++ src/components/Flight/index.jsx
@@ -2,18 +2,18 @@ import "./index.css";
 
 export const Flight = (props) => {
     return(
-        <div class="flight">
-            <div class="flight__icon"></div>
-            <div class="flight__info">
-                <div class="flight__stop">
-                <p class="flight__stop-type">Odlet:</p>
-                <p class="flight__stop-city">{props.cityFrom}</p>
-                <p class="flight__stop-time">{props.timeFrom}</p>
+        <div className="flight">
+            <div className="flight__icon"></div>
+            <div className="flight__info">
+                <div className="flight__stop">
+                <p className="flight__stop-type">Odlet:</p>
+                <p className="flight__stop-city">{props.cityFrom}</p>
+                <p className="flight__stop-time">{props.timeFrom}</p>
                 </div>
-                <div class="flight__stop">
-                <p class="flight__stop-type">Přílet:</p>
-                <p class="flight__stop-city">{props.cityTo}</p>
-                <p class="flight__stop-time">{props.timeTo}</p>
+                <div className="flight__stop">
+                <p className="flight__stop-type">Přílet:</p>
+                <p className="flight__stop-city">{props.cityTo}</p>
+                <p className="flight__stop-time">{props.timeTo}</p>
                 </div>
             </div>
         </div>
